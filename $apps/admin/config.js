02d7a3b6c0d002bc0e9a467f36947edf4a2889af
zengjin <zengjin@88.com>
2026-04-14 19:27:41 +0800

    admin通用

--- $apps/admin/config.js
+++ $apps/admin/config.js
@@ -1,5 +1,9 @@
 window.$config = {
 	...window.$config,
-	app: 'admin',
-	title: '后台管理系统',
+	app: 'admin', // 应用唯一标识
+	title: '后台管理系统', // 浏览器标题
+	keepAlive: true, // 是否开启组件缓存
+	multiTabs: true, // 是否开启多标签页
+	loginStrict: true, // 是否强制登录
+	publicLayout: true, // 是否公开可见完整框架布局 (false 时仅 admin 可见, 未登录或其他用户仅能看见具体页面内容)
 }
--- $apps/gis/config.js
+++ $apps/gis/config.js
@@ -1,5 +1,9 @@
 window.$config = {
 	...window.$config,
-	app: 'gis',
-	title: 'GIS实例',
+	app: 'gis', // 应用唯一标识
+	title: 'GIS实例', // 浏览器标题
+	keepAlive: false, // 是否开启组件缓存
+	multiTabs: false, // 是否开启多标签页
+	loginStrict: false, // 是否强制登录
+	publicLayout: false, // 是否公开可见完整框架布局 (false 时仅 admin 可见, 未登录或其他用户仅能看见具体页面内容)
 }
